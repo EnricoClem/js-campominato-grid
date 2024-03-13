1c8cdef1b00055de546bf054cacf8175e0899c2b
EnricoClem <enrico.clem94@gmail.com>
2024-03-13 17:36:21 +0100

    ADD - js button start listening

---
+++ app/app.js
@@ -0,0 +1,20 @@
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+console.log('Campominato.')
+
+// Collegare con il bottone start
+const startGame = document.getElementById('start-button')
+
+// Ascoltare azione di click su bottone start
+startGame.addEventListener('click', function () {
+    console.log('Inizio gioco!')
+
+    // Generare 100 celle all'interno della griglia
+
+    // Permettere di cambiare colore alle celle al click
+
+})
+
